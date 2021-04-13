1a962e7ec90b06e6319e007e6dba270d5f39239f
Max <maxtarasevich@gmail.com>
2021-04-13 18:48:35 +0300

    Header up

--- omdb/src/component/App.js
+++ omdb/src/component/App.js
@@ -84,7 +84,7 @@ const App = () => {
 
     return (
     <div className="App">
-      <Header text="HOOKED" />
+      <Header text="OmdbSearch" />
       <Search search={search} />
       <p className="App-intro">Sharing a few of our favourite movies</p>
       <div className="movies">
